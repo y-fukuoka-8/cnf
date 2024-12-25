df4d12286b5b60f8721897bdb15cb941bce5a607
y-fukuoka-8 <y-fukuoka@fujitsu.com>
2024-12-25 10:22:32 +0900

    Update test_ex.py

--- test_ex.py
+++ test_ex.py
@@ -6,9 +6,6 @@ from ex import ex_funcs
     "x, y, expected",
     [
         (3, 2, 1),
-        (2, 3, 5),
-        (10, 20, 30),
-        # 　かおかおかおかおかおこ
     ],
 )
 def test_ex_funcs(x: int, y: str, expected: int) -> None:
